8eafdac83f895146688a976642970ead9aa0c2aa
wanjocharles <wanjocharles98@gmail.com>
2023-06-19 05:19:05 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@ char a;
 int b;
 long int c;
 long long int d;
-float f;
-printf("Size of a char: 1 byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int : 4 byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int:4 byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: 8 byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: 4 byte(s)\n", (unsigned long)sizeof(f));
+float e;
+printf("Size of a char: %lu\n byte(s)", (unsigned long)sizeof(a));
+printf("Size of an int : %lu\n byte(s)", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu\n byte(s)", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu\n byte(s)", (unsigned long)sizeof(d));
+printf("Size of a float: %lu\n byte(s)", (unsigned long)sizeof(e));
 return (0);
 }
