41b2dde7cc35b69855dd7afd153e29be0569967c
wanjocharles <wanjocharles98@gmail.com>
2023-06-21 10:48:58 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prog Entry point
- * Retrun: Always 0 (success)
+ * main - prog prints all possible different combinations of two digits
+ * Return: 0 (success)
  */
 int main(void)
 {
