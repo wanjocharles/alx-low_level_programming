07795ab53ea32c8aa270aab8f1c65a17e63d101c
wanjocharles <wanjocharles98@gmail.com>
2023-06-21 10:43:23 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prog Entry point
+ * Retrun: Always o (success)
+ */
+int main(void)
+{
+int ones = '0';
+int tens = '0';
+for (tens = '0'; tens <= '9'; tens++)
+{
+for (ones = '0'; ones <= '9'; ones++)
+{
+if (!((ones == tens) || (tens > ones)))
+{
+putchar(tens);
+putchar(ones);
+if (!(ones == '9' && tens == '8'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
