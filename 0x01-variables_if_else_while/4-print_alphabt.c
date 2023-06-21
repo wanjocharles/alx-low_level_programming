beb5b1d8d120a721e541dc9e7327daaccf1211f3
wanjocharles <wanjocharles98@gmail.com>
2023-06-21 07:47:44 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prog Entry point
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+char c;
+c = 'a';
+while
+(c <= 'z')
+{
+if ((c != 'q' && c != 'e') && c <= 'z')
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
