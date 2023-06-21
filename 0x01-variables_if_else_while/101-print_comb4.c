d7bb7a0b91f455a6e4d356c569414b6f9c7bb025
wanjocharles <wanjocharles98@gmail.com>
2023-06-21 11:47:21 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ for (p = d + 1; p <= '9'; p++)
 for (q = p + 1; q <= '9'; q++)
 {
 if ((p != d) != q)
-putchar(d);
+{putchar(d);
 putchar(p);
 putchar(q);
 if (d == '7' && p == '8')
@@ -27,5 +27,5 @@ putchar(' ');
 }
 }
 putchar('\n');
-return (0);
+return(0);
 }
