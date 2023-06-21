776475f7ac945bc29633f2aa118070c5ee420aad
wanjocharles <wanjocharles98@gmail.com>
2023-06-21 13:59:31 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print _alphabet - make alphabet
+ * Return: void
+ */
+void print _alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+
