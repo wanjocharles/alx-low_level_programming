99c23e82feda57f3e9667d622677aad659577f4d
wanjocharles <wanjocharles98@gmail.com>
2023-06-21 12:13:29 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prog entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int p, q;
+{
+for (p = 0; p <= 98; p++)
+{
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
+putchar(' ');
+putchar((q / 10) + '0');
+putchar((q % 10) + '0');
+if (p == 98 && q == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
