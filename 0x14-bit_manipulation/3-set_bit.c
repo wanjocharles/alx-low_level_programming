495a3a8708ac1a37760601a6832c1bce614a1fc1
wanjocharles <wanjocharles98@gmail.com>
2023-10-06 14:45:06 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  * @index: index of the bit to set to 1
  * Return: 1 for succes, -1 for failure
  */
-int set_bit(unsigned long int *n, unisgned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 if (index > 63)
 return (-1);
