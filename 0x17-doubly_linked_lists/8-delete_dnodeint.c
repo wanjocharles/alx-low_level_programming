447a4313d3d0a46655b711c984df5a342ba6f325
wanjocharles <wanjocharles98@gmail.com>
2023-12-07 11:36:05 -0800

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,7 +5,7 @@
  *
  * @head: head of the list
  * @index: index of the new node
- * return: 1 if it succeeded, -1 if it failed
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
